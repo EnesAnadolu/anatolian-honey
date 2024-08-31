67fd021db4d5187f224b7d6765d96c345ea55963
BartuPacall <Bartup.123a@hotmail.com>
2024-08-31 18:24:15 +0300

    Başlık silindi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
         <h3 className="font-weight:bold">Organic Honey</h3>
         <p className="custom-font-body">Very organic, delicious bee products</p>
         <br />
-        <h4>Bartu Melissayı seviyor</h4>
         <Image
           width={100}
           height={100}
