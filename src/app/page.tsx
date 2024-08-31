bd2e2080b17d05e4a0434525ef5b8a1142bd7ebe
EnesAnadolu <103136842+EnesAnadolu@users.noreply.github.com>
2024-08-31 18:00:48 +0300

    fixed: Image issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="ml-40 ">
         <h4 className="custom-font-headline text-5xl">Hello World</h4>
         <p className="custom-font-body">Very organic, delicious bee products</p>
-        <Image src={"https://www.maeshoney.com/wp-content/uploads/2021/07/colmena.jpg"} alt="bees"/>
+        <Image width={100} height={100} src={"https://www.maeshoney.com/wp-content/uploads/2021/07/colmena.jpg"} alt="bees"/>
       </div>
       
     </main>
